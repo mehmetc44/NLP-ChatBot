e8a5278732ef33ffb8a25a40910e499624e3e65a
KurtTolga04 <kurttolga40@gmail.com>
2025-06-10 20:36:31 +0300

    rasa updated

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -1,23 +1,10 @@
-# This file contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these actions:
-# https://rasa.com/docs/rasa/custom-actions
-
-from typing import Any, Text, Dict, List
-
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import Action
 
 
-class ActionHelloWorld(Action):
+class ActionWelcome(Action):
+    def name(self):
+        return "action_welcome"
 
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(text="Hello, I am Natasha. How can I help you?")
         return []
